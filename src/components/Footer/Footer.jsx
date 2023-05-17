72ae200febee77589384a6a42900dc9e3bc8a41f
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-18 00:37:57 +0600

    Footer Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,23 @@
-
+import Logo from "../../assets/ball.png"
+import {FaFacebook,FaInstagram,FaYoutube} from "react-icons/fa"
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className="footer footer-center p-10 bg-purple-500 text-white">
+            <div>
+             <img src={Logo} className="w-24 h-24" alt="" />
+                <p className="font-bold">
+                    Toy Castle <br />Providing best products since 1992
+                </p>
+                <p>Copyright ©Toycastle 2023 - All right reserved</p>
+            </div>
+            <div>
+                <div className="grid  grid-cols-3 gap-6">
+                   <FaFacebook className="w-8 h-8"/>
+                   <FaInstagram className="w-8 h-8"/>
+                   <FaYoutube className="w-9 h-9"/>
+                </div>
+            </div>
+        </footer>
     );
 };
 
