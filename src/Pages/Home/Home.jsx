5f17208aa5d98526241fe504b28fbd6be1f68176
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-18 12:04:11 +0600

    shopbycategory section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner/Banner";
 import Gallery from "../../components/Gallery/Gallery";
+import ShopByCategory from "../../components/ShopByCategory/ShopByCategory";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Gallery/>
+            <ShopByCategory/>
         </div>
     );
 };
