3e1e9bc9b7781f24e0309cf33aaeb624314d2dc6
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-19 15:51:15 +0600

    fixed bug on private route

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,24 +1,18 @@
-/* eslint-disable react/prop-types */
 import { useContext } from "react"
 import { Navigate, useLocation } from "react-router-dom";
 import { FirebaseAuthContext } from "../Provider/FirebaseAuthProvider";
+// eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
     const { loading, user } = useContext(FirebaseAuthContext)
     const location = useLocation()
     if (loading) {
         return <p className="text-center text-lg font-bold py-12">Loading...</p>
     }
-   
-    if (!user) {
-        return (
-          <>
-            <p className="text-center font-bold text-5xl">Please login to see details</p>
-            <Navigate to="/login" state={{ from: location }} replace />
-          </>
-        );
-      }
-    
-      return children;
-    };
+    if (user) {
+        return children
+    }
+    return <Navigate to="/login" state={{from:location}} replace={true}></Navigate>
+};
+
 
 export default PrivateRoute;
