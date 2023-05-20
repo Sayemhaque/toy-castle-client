8484a728d1c127d5f563db872325be8b67165805
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-20 21:05:51 +0600

    changes photos

--- src/components/Exclusive Toys/ExclusiveToys.jsx
+++ src/components/Exclusive Toys/ExclusiveToys.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-
+import ExclusiveBanner from "../../assets/_Blue White Modern Marketing Strategy Blog Banner.jpg"
+import AOS from "aos";
+import 'aos/dist/aos.css'
 const ExclusiveToys = () => {
   const [photos, setPhotos] = useState([])
   const [loading,setLoading] = useState(false)
 
   useEffect(() => {
+    AOS.init()
       const getPhosts = async () => {
         setLoading(true)
           const res = await fetch("https://railway-server-production-a1a3.up.railway.app/exclusive");
@@ -17,10 +20,10 @@ const ExclusiveToys = () => {
   console.log(photos)
     return (
         <div className="md:max-w-6xl mx-auto py-12 px-5">
-              <h1 className='font-bold text-2xl md:text-4xl text-center font-serif mb-12'>Exclusive Toys</h1>
+              <h1 className='font-bold text-2xl md:text-4xl text-center font-serif mb-12 '>Exclusive Toys</h1>
              {loading &&  <p className="text-center">Loading...</p>}
             <div className="grid grid-cols-2 md:grid-cols-4 gap-5 space-y-12">
-            <img className="col-span-2 md:col-span-4" src="https://cdn.shopify.com/s/files/1/0088/7986/5907/files/Wooden_Toys_1230x410.jpg?v=1681476513" alt="" />
+            <img className="col-span-2 md:col-span-4  w-full object-cover" data-aos="fade-up-right" src={ExclusiveBanner} alt="" />
               {photos.map((photo) => {
                 return <>
                  <div>
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -18,7 +18,7 @@ const Gallery = () => {
     }, [])
     console.log(photos)
     return (
-        <div className="py-12 px-5">
+        <div className="py-12 px-5 md:max-w-6xl mx-auto">
             <h2 className="font-bold text-center text-2xl md:text-5xl font-serif">Gallery</h2>
             {loading && <p className="text-center text-xl  mt-5 font-bold font-serif">Loading...</p>}
             <div className="grid grid-cols-2 md:grid-cols-3 gap-5 py-5 place-items-center">
